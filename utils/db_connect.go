42463258769fb82bfbe224aeba4ab8fc6b66d89d
kurniawanxyz <kaitodecode@gmail.com>
2024-09-08 21:49:47 +0700

    feat: env

--- utils/db_connect.go
+++ utils/db_connect.go
@@ -1,14 +1,26 @@
 package utils
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func DBConnect() (db *gorm.DB, err error) {
-	dsn := "root:2006@tcp(127.0.0.1:3306)/utschool_db?charset=utf8mb4&parseTime=True&loc=Local"
+	errenv := godotenv.Load()
+	if errenv != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, dbName)
 	db, error := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if error != nil {
 		log.Fatal("Failed to connect database", err)
